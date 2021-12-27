a6a904d142797efc56a27b1cbeb0f2bccecee2ca
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-12-27 07:57:53 +0700

    211227 - BLTAnh - Dotenv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
+require('dotenv').config({ path: './.env' });
 require('./middlewares/handlebars')(app);
 
 const userRouter = require('./routes/userRoute');
