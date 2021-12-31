49ce58ca1bde2388745f0a0c5ef3cc470ef059a5
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-12-31 10:02:15 +0700

    211231 - BLTAnh - Some changes

--- models/packageModel.js
+++ models/packageModel.js
@@ -1,12 +1,30 @@
 const factory = require('../utils/handlerFactory');
-
 const TableName = "Packages";
-const packID = _f_ID, packName = _f_Name, pack = _package;
 
-exports.getAllPackages = factory.getAll(TableName);
-exports.getPackageById(packID) = factory.getOne(TableName, 'f_ID', packID);
-exports.getPackageByName(packName) = factory.getOne(TableName, 'f_Name', packName);
-exports.searchPackageByName(packName) = factory.search(TableName, 'f_Name', packName);
-exports.addPackage(pack) = factory.createOne(TableName, pack);
-exports.editPackage(packID, pack) = factory.updateOne(TableName, 'f_ID', packID, pack);
-exports.deletePackage(packID) = factory.deleteOne(TableName, 'f_ID', packID);
+exports.getAllPackages = async () => {
+    return await factory.getAll(TableName);
+}
+
+exports.getPackagesByName = async (_f_Name) => {
+    return await factory.getOne(TableName, 'f_Name', _f_Name);
+}
+
+exports.getPackagesById = async (_f_ID) => {
+    return await factory.getOne(TableName, 'f_ID', _f_ID);
+}
+
+exports.searchPackagesByName = async (_f_Name) => {
+    return await factory.search(TableName, 'f_Name', _f_Name);
+}
+
+exports.addPackage = async (_package) => {
+    return await factory.createOne(TableName, _package);
+}
+
+exports.editPackage = async (_f_ID, _package) => {
+    return await factory.updateOne(TableName, 'f_ID', _f_ID, _package);
+}
+
+exports.deletePackage = async (_f_ID) => {
+    return await factory.deleteOne(TableName, 'f_ID', _f_ID);
+}
--- models/productModel.js
+++ models/productModel.js
@@ -1,11 +1,29 @@
 const factory = require('../utils/handlerFactory');
-
 const TableName = "Products";
 
-exports.getAllProducts = factory.getAll(TableName);
-exports.getProductByName(prodName) = factory.getOne(TableName, 'f_Name', prodName);
-exports.getProductById(prodId) = factory.getOne(TableName, "f_ID", prodId);
-exports.searchProductByName(prodName) = factory.search(TableName, 'f_Name', prodName);
-exports.addProduct(product) = factory.createOne(TableName, product);
-exports.editProduct(product) = factory.updateOne(TableName, prodId, product);
-exports.deleteProduct(prodId) = factory.deleteOne(TableName, 'f_ID', prodId);
+exports.getAllProducts = async () => {
+    return await factory.getAll(TableName);
+}
+
+exports.getProductByName = async (_f_Name) => {
+    return await factory.getOne(TableName, 'f_Name', _f_Name);
+}
+
+exports.getProductById = async (_f_ID) => {
+    return await factory.getOne(TableName, "f_ID", _f_ID);
+}
+exports.searchProductByName = async (_f_Name) => {
+    return await factory.search(TableName, 'f_Name', _f_Name);
+}
+
+exports.addProduct = async (_product) => {
+    return await factory.createOne(TableName, _product);
+}
+
+exports.editProduct = async (_f_ID, _product) => {
+    return await factory.updateOne(TableName, 'f_ID', _f_ID, _product);
+}
+
+exports.deleteProduct = async (_f_ID) => {
+    return await factory.deleteOne(TableName, 'f_ID', _f_ID);
+}
