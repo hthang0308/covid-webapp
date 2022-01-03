e4a7c5119c8293c67cdacdf67342aae4c1517f05
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2022-01-03 19:22:19 +0700

    220103 - BLTAnh - Multer & Chia tách hệ thống giai đoạn 1

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,11 +1,12 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads');
-    }, filename: function (req, file, cb) {
-        cb(null, file.fieldname + '_' + Date.now())
+    filename: function (req, file, cb) {
+        const extArray = file.mimetype.split('/');
+        const ext = extArray[extArray.length - 1];
+        cb(null, file.fieldname + '-' + Date.now() + '.' + ext);
     }
 })
 
 var uploads = multer({ storage: storage });
+module.exports = uploads;
