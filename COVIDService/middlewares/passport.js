a4dcbb3143151f75de37152036f72af78850052f
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2022-01-08 14:56:52 +0700

    220108 - BLTAnh - Major changes

---
+++ COVIDService/middlewares/passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
+
+const userModel = require('../models/user.M');
+
+passport.use(new LocalStrategy(
+    async function (username, password, done) {
+        const user = await userModel.checkCorrectAccount(username, password);
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username or password' });
+        }
+        return done(null, user);
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    userModel.getUser(id)
+        .then((user) => {
+            done(null, user);
+        })
+});
+
+module.exports = passport;
