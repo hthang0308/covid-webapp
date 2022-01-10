9088df30608842656f85834668c4ed1f09132728
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2022-01-10 08:25:09 +0700

    220110 - BLTAnh - AuthCtrl

--- COVIDService/routes/packageRoute.js
+++ COVIDService/routes/packageRoute.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const packCtrl = require('../controllers/package.C')
-
+const authCtrl = require('../controllers/auth.C');
 const router = express.Router()
 
+// Login to access
+router.use(authCtrl.protect);
+
+// Manager access
+router.use(authCtrl.restrictTo('manager'))
 router.get('/', packCtrl.getAllPackages);
 router.route('/:id').get(packCtrl.getPackage).patch(packCtrl.editPackage).delete(packCtrl.deletePackage);
 router.post('/add', packCtrl.createPackage);
--- COVIDService/routes/userRoute.js
+++ COVIDService/routes/userRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const userCtrl = require("../controllers/user.C");
+const authCtrl = require("../controllers/auth.C");
 
 const router = express.Router();
 
+// Login to access
+router.use(authCtrl.protect);
+
+// Manager roles
+router.use(authCtrl.restrictTo('manager'));
 router.route("/").get(userCtrl.getAllUsers).post(userCtrl.createUser);
 router.get("/search", userCtrl.searchUser); //user/search?id=2333
 router.get("/create", userCtrl.getCreateForm);
