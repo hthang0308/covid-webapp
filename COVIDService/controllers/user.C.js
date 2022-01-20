6ec87f89191f3d26ac03cdd35ea20e55774b29c7
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2022-01-20 20:02:11 +0700

    220120 - BLTAnh - Some changes to route

--- COVIDService/controllers/user.C.js
+++ COVIDService/controllers/user.C.js
@@ -54,7 +54,10 @@ exports.getBalance = async (req, res) => {
   if (token) {
     data = await userModel.getPayment(id, token);
   }
-  return res.json(data);
+  res.render('normaluser/balance', {
+    title: 'Số dư hiện tại',
+    data: data.json(data)
+  })
 };
 
 exports.deposit = async (req, res) => {
@@ -149,9 +152,8 @@ exports.createUser = async (req, res) => {
   const qls = await userModel.getAllQL();
   if (err !== "") return res.render("users/form_adduser", { layout: "manager", data: req.body, cities, qls, err });
   var currentDate = new Date();
-  var currentTime = `${currentDate.getDay()}/${
-    currentDate.getMonth() + 1
-  }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `;
+  var currentTime = `${currentDate.getDay()}/${currentDate.getMonth() + 1
+    }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `;
   const newUser = {
     f_Username: req.body.username,
     f_Password: "",
@@ -234,9 +236,8 @@ exports.editUser = async (req, res) => {
   const tmpUser = await userModel.getUserByID(req.params.id);
   if (tmpUser === undefined) return;
   var currentDate = new Date();
-  var currentTime = `${currentDate.getDay()}/${
-    currentDate.getMonth() + 1
-  }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `; //If in form has covid address
+  var currentTime = `${currentDate.getDay()}/${currentDate.getMonth() + 1
+    }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `; //If in form has covid address
   if (req.body.ql !== undefined) {
     tmpUser.f_QuarantineID = req.body.ql;
     const tmpNewCovidAddress = await userModel.getQLByID(tmpUser.f_QuarantineID);
