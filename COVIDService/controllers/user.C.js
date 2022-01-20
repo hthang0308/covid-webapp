c44f47859dcafa6a5fb236567bf0a2a18ade50f6
Thang <hthang0308a@gmail.com>
2022-01-20 11:06:44 +0700

    fix product, package,...

--- COVIDService/controllers/user.C.js
+++ COVIDService/controllers/user.C.js
@@ -19,8 +19,8 @@ exports.getAllUsers = async (req, res) => {
   if (req.query.sort === "name") sort.sortByFullName(arr);
   res.render("users/all", {
     users: arr,
-    title: 'Danh sách người liên quan COVID-19',
-    layout: 'manager',
+    title: "Danh sách người liên quan COVID-19",
+    layout: "manager",
   });
 };
 
@@ -29,8 +29,8 @@ exports.searchUser = async (req, res) => {
   if (tmpUser === undefined) return;
   res.render("users/all", {
     users: tmpUser,
-    title: 'Tìm kiếm người liên quan COVID-19',
-    layout: 'manager',
+    title: "Tìm kiếm người liên quan COVID-19",
+    layout: "manager",
   });
 };
 
@@ -80,10 +80,11 @@ exports.createUser = async (req, res) => {
 
   const cities = await userModel.getAllCities();
   const qls = await userModel.getAllQL();
-  if (err !== "") return res.render("users/form_adduser", { layout: "tmp", data: req.body, cities, qls, err });
+  if (err !== "") return res.render("users/form_adduser", { layout: "manager", data: req.body, cities, qls, err });
   var currentDate = new Date();
-  var currentTime = `${currentDate.getDay()}/${currentDate.getMonth() + 1
-    }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `;
+  var currentTime = `${currentDate.getDay()}/${
+    currentDate.getMonth() + 1
+  }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `;
   const newUser = {
     f_Username: req.body.username,
     f_Password: "",
@@ -107,7 +108,7 @@ exports.createUser = async (req, res) => {
   if (!result) {
     err = "Can't add this user";
   }
-  if (err !== "") return res.render("users/form_adduser", { layout: "tmp", data: req.body, cities, qls, err });
+  if (err !== "") return res.render("users/form_adduser", { layout: "manager", data: req.body, cities, qls, err });
 
   if (source) {
     console.log(source.f_RelatedID);
@@ -117,7 +118,7 @@ exports.createUser = async (req, res) => {
     }
     await userModel.editUser(source.f_ID, source);
   }
-  return res.render("users/form_adduser", { layout: "tmp", cities, data: req.body, qls, err: "Successfully" });
+  return res.render("users/form_adduser", { layout: "manager", cities, data: req.body, qls, err: "Successfully" });
   //done
 };
 
@@ -165,8 +166,9 @@ exports.editUser = async (req, res) => {
   const tmpUser = await userModel.getUserByID(req.params.id);
   if (tmpUser === undefined) return;
   var currentDate = new Date();
-  var currentTime = `${currentDate.getDay()}/${currentDate.getMonth() + 1
-    }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `; //If in form has covid address
+  var currentTime = `${currentDate.getDay()}/${
+    currentDate.getMonth() + 1
+  }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()} `; //If in form has covid address
   if (req.body.ql !== undefined) {
     tmpUser.f_QuarantineID = req.body.ql;
     const tmpNewCovidAddress = await userModel.getQLByID(tmpUser.f_QuarantineID);
